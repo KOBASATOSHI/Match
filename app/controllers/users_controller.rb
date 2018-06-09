71b14e5a819b9bcd3719a3a3563f49e5302da1e8
Yot0201412 <laughing.my.way0201@gmail.com>
2018-06-09 12:12:39 +0000

    basic_login_system

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user ,only:[:show, :index , :edit, :update , :destroy]
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:info] = "Succeeded to Create your account!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+     @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name,
+        :email ,
+        :password ,
+        :password_confirmation)
+    end
+end
