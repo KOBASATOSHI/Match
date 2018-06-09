4baa2fbd16f67da2d90e47fb41268e05441cf1ca
Yot0201412 <laughing.my.way0201@gmail.com>
2018-06-09 08:40:39 +0000

    add auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 class User < ApplicationRecord
     before_save   { email.downcase! }
-    before_create :create_activation_digest
     validates :name , presence: true , length: {maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email , presence: true, length: {maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
              uniqueness:{case_sensitive:false}
+    has_secure_password
+    validates :password , presence: true ,length:{ minimum: 6 } ,allow_nil: true
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 end
